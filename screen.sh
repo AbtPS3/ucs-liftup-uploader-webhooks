478177e4d1c63bd368a5d181e6b49144fa52044d
Kizito Mrema <kizomanizo@gmail.com>
2024-01-21 20:16:02 +0300

    MAJOR: Changed killing attitude

--- screen.sh
+++ screen.sh
@@ -8,7 +8,7 @@ sleep 3
 # Check if the screen session "webhooks" is already running
 if screen -ls | grep -q "webhooks"; then
     # If it is running, reattach to the existing session, stop nodemon, and then run deploy.sh
-    screen -S webhooks -X stuff 'bash ./deploy.sh\n'
+    screen -S webhooks -X stuff 'screen -S webhooks -X stuff "^C\n && sleep 2 && ./deploy.sh\n"'
 else
     # If it is not running, start a new session and execute the deploy script
     screen -S webhooks -d -m bash -c 'bash ./deploy.sh'
