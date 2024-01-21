076283bac76ea13e7cc06e8ac2e57622ec8d8b44
Kizito Mrema <kizomanizo@gmail.com>
2024-01-21 19:18:24 +0300

    MINOR: Added bash to command

--- screen.sh
+++ screen.sh
@@ -3,8 +3,8 @@
 # Check if the screen session "webhooks" is already running
 if screen -ls | grep -q "webhooks"; then
     # If it is running, reattach to the existing session
-    screen -r webhooks -X stuff './deploy.sh\n'
+    screen -r webhooks -X stuff 'bash ./deploy.sh\n'
 else
     # If it is not running, start a new session and execute the deploy script
-    screen -S webhooks -d -m bash -c './deploy.sh'
+    screen -S webhooks -d -m bash -c 'bash ./deploy.sh'
 fi
