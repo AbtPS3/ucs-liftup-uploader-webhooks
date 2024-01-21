9b1f0c65fb190e391d6f50ab980d47dd7bdd9b48
Kizito Mrema <kizomanizo@gmail.com>
2024-01-21 19:43:16 +0300

    BUG FIX: Adden new line chars

--- screen.sh
+++ screen.sh
@@ -3,9 +3,12 @@
 # Check if the screen session "webhooks" is already running
 if screen -ls | grep -q "webhooks"; then
     # If it is running, reattach to the existing session, stop nodemon, and then run deploy.sh
-    screen -S webhooks -X stuff $'pkill -f "nodemon"\n'
-    screen -S webhooks -X stuff $'./deploy.sh\n'
+    screen -S webhooks -X stuff $'pkill -f "nodemon"
+    '
+    screen -S webhooks -X stuff $'./deploy.sh
+    '
 else
     # If it is not running, start a new session and execute the deploy script
-    screen -S webhooks -d -m bash -c 'pkill -f "nodemon" && ./deploy.sh'
+    screen -S webhooks -d -m bash -c 'pkill -f "nodemon" && ./deploy.sh
+    '
 fi
