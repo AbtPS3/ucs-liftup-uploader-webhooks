d2d94c2f17b09b23af346e43c54c5e96aff52a22
Kizito Mrema <kizomanizo@gmail.com>
2024-01-21 20:29:01 +0300

    MINOR: Touched up bash comments

--- screen.sh
+++ screen.sh
@@ -8,7 +8,7 @@ sleep 1
 # Check if the screen session "webhooks" is already running
 if screen -ls | grep -q "webhooks"; then
     # If it is running, reattach to the existing session, stop nodemon, and then run deploy.sh
-    screen -S webhooks -X stuff $'\003 && ./deploy.sh' # Sending Ctrl+C (SIGINT)
+    screen -S webhooks -X stuff $'\003 && ./deploy.sh' # Sending Ctrl+C (SIGINT and bash)
     sleep 2
     screen -S webhooks -X stuff './deploy.sh\n'
 else
