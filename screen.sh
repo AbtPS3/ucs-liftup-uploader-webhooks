59cfab75394e632ae83bc7574d3533385cac0c76
Kizito Mrema <kizomanizo@gmail.com>
2024-01-21 19:49:53 +0300

    MINOR: Added pkill before screen session

--- screen.sh
+++ screen.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Kill Nodemon First
+pkill -f "nodemon"
+
 # Check if the screen session "webhooks" is already running
 if screen -ls | grep -q "webhooks"; then
     # If it is running, reattach to the existing session, stop nodemon, and then run deploy.sh
-    screen -S webhooks -X stuff 'pkill -f "nodemon" && sleep 1 && ./deploy.sh\n'
+    screen -S webhooks -X stuff 'bash ./deploy.sh\n'
 else
     # If it is not running, start a new session and execute the deploy script
-    screen -S webhooks -d -m bash -c 'pkill -f "nodemon" && sleep 1 && ./deploy.sh'
+    screen -S webhooks -d -m bash -c 'bash ./deploy.sh'
 fi
