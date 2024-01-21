9bef1aedc4d6cbcbc4ab625980f017cbede6bf32
Kizito Mrema <kizomanizo@gmail.com>
2024-01-21 19:24:05 +0300

    Minor changes to add delay

--- screen.sh
+++ screen.sh
@@ -2,9 +2,10 @@
 
 # Check if the screen session "webhooks" is already running
 if screen -ls | grep -q "webhooks"; then
-    # If it is running, reattach to the existing session
-    screen -r webhooks -X stuff 'bash ./deploy.sh\n'
+    # If it is running, reattach to the existing session and send the command
+    screen -S webhooks -X stuff $'./deploy.sh\n'
+    sleep 1
 else
     # If it is not running, start a new session and execute the deploy script
-    screen -S webhooks -d -m bash -c 'bash ./deploy.sh'
+    screen -S webhooks -d -m bash -c './deploy.sh'
 fi
